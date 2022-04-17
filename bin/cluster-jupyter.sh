3a2100b062dfbb874bc180cac454ab400e3993f1
ConradScott <ConradScott@users.noreply.github.com>
2022-04-17 19:36:14 +0100

    Name the Jupyter container

--- bin/cluster-jupyter.sh
+++ bin/cluster-jupyter.sh
@@ -6,4 +6,9 @@ set -x
 
 # Don't use `--init` as this image uses `tini`
 #
-docker run -it --rm -p 8888:8888/tcp -v jupyter:/home/jovyan/work --network ioj ioj-jupyter:latest
+docker run -it --rm \
+  --name ioj-jupyter \
+  -v jupyter:/home/jovyan/work \
+  --network ioj \
+  -p 8888:8888/tcp \
+  ioj-jupyter:latest
